06537d12492265ee515012081e029ac7acb786b1
Petter A. Urkedal <paurkedal@gmail.com>
2017-07-01 22:42:07 +0200

    Switch to jbuilder.

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -1,18 +1,9 @@
 #! /usr/bin/env ocaml
 #use "topfind"
-#require "topkg"
-open Topkg
+#require "topkg-jbuilder"
 
-let with_ipaddr = Conf.with_pkg "ipaddr"
+open Topkg
 
 let licenses = List.map Pkg.std_file ["COPYING.LESSER"; "COPYING"]
 
-let () = Pkg.describe ~licenses "radixmap" @@ fun c ->
-  Ok [
-    Pkg.mllib "lib/radixmap.mllib";
-    Pkg.mllib ~cond:(Conf.value c with_ipaddr) "lib/radixmap-ipaddr.mllib";
-    Pkg.test "tests/test_bitword";
-    Pkg.test "tests/test_bitword_radixmap";
-    Pkg.test "tests/test_ip_radixset";
-    Pkg.test "tests/test_ip_radixmap";
-  ]
+let () = Topkg_jbuilder.describe ~licenses ()
